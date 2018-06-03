64c11e2b14465214315529c29456b706ea32c89b
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-06-03 17:23:37 -0300

    article component complete

---
+++ app/javascript/components/Article.js
@@ -0,0 +1,48 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Timestamp from 'react-timestamp';
+
+class Article extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+          <div className="article-title">
+              <a href={this.props.path}>{this.props.title}</a>
+          </div>
+          <div className="article-body">
+              {this.props.description}
+
+              <div className="article-meta-details">
+
+                <small>Created by: { this.props.author }, <Timestamp time={this.props.created_at} precision={4} />
+                  , last updated: <Timestamp time={this.props.updated_at} precision={4} />
+              </small>
+              </div>
+          </div>
+      </React.Fragment>
+    );
+  }
+
+  componentDidMount() {
+      var self = this;
+      this._timer = setInterval(function() { self.forceUpdate(); }, 5000);
+  }
+
+  componentWillUnmount() {
+      if (this._timer) {
+          clearInterval(this._timer);
+          this._timer = null;
+      }
+  }
+}
+
+Article.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  path: PropTypes.string,
+  author: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string
+};
+export default Article
